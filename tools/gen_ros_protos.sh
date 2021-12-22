034e5ecbe3c364f25886fa684070397f022b748d
Daniel Chia <danstryder@gmail.com>
2021-12-21 23:31:45 -0800

    remove websocket server expt

--- tools/gen_ros_protos.sh
+++
@@ -1,5 +0,0 @@
-PROTO_PATH=$HOME/projects/ros-message-schemas/proto
-protoc -I=$PROTO_PATH --python_out=ros_msgs  $PROTO_PATH/ros/**/*.proto $PROTO_PATH/ros/*.proto
-protoc -I=$PROTO_PATH $PROTO_PATH/ros/**/*.proto $PROTO_PATH/ros/*.proto --include_imports --descriptor_set_out=ros_msgs/descriptor.bin
-# DESCRIPTOR_PATH=ros_msgs/descriptors
-# protoc -I=$PROTO_PATH ros/sensor_msgs/CompressedImage.proto --include_imports --descriptor_set_out=$DESCRIPTOR_PATH/CompressedImage.bin
