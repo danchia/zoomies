d164a8363ef8005fdd92822a49a546cfde124c93
Daniel Chia <danstryder@gmail.com>
2021-12-26 22:58:35 -0800

    update cam tools

--- tools/camera_table.py
+++ tools/camera_table.py
@@ -10,6 +10,7 @@ K[:2] /= 4.05
 uv = np.mgrid[:480, :640][[1, 0]].transpose(1, 2, 0).astype(np.float32)
 pts = cv2.fisheye.undistortPoints(uv.reshape(640*480, 1, 2), K, D)
 pts = pts.reshape(480*640, 2)
+pts_cam = pts
 pts = np.append(pts, np.ones((480*640, 1)), axis=1)
 
 R, _ = cv2.Rodrigues(np.array([0, np.radians(29), 0], dtype=np.float32))
@@ -17,11 +18,22 @@ pts = np.dot(pts, R.transpose())
 
 # pts = pts / np.linalg.norm(pts, axis=1, keepdims=True)
 # normalize z=1
-pts = pts / pts[:, 2][:, None]
+pts = pts / np.absolute(pts[:, 2][:, None])
 np.save('../data/calib/camera_lut.npy', pts, False)
 
-pts = pts[:, 0:2]
-pts = pts.reshape(480*640*2).astype(np.float32)
+lut = pts[:, 0:2]
+lut_1d = lut.reshape(480*640*2).astype(np.float32)
 f = open('../data/calib/camera_lut.bin', 'wb')
-f.write(pts.tobytes())
+f.write(lut_1d.tobytes())
+f.close()
+
+ceil_mask = np.zeros(480*640, dtype=np.uint8)
+# * We want ceiling, so mask out floor (neg z).
+# * OpenCV fisheye can't handle past 180, and close to 180 deg it's quite poor. acos(1/10) ~ 84deg
+# * Also, mask out really far ceiling pts. tan(70deg) ~ 2.8
+ceil_mask[(pts[:, 2] > 0) & (np.sum(
+    pts_cam**2, axis=1) < 100) & (np.sum(lut**2, axis=1) < 2.8**2)] = 255
+
+f = open('../data/calib/ceil_mask.bin', 'wb')
+f.write(ceil_mask.reshape(480*640).tobytes())
 f.close()
