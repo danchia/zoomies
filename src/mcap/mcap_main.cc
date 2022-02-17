cbc1d437716c892dcd142d574b1fe4d0b663aa44
Daniel Chia <danstryder@gmail.com>
2022-02-17 04:39:12 -0500

    test complex msg

--- src/mcap/mcap_main.cc
+++ src/mcap/mcap_main.cc
@@ -3,8 +3,10 @@
 #include <fstream>
 #include <iostream>
 #include <mcap/mcap.hpp>
+#include <unordered_set>
 
 #include "google/protobuf/descriptor.pb.h"
+#include "ros/geometry_msgs/PointStamped.pb.h"
 #include "ros/std_msgs/String.pb.h"
 
 // Returns the system time in nanoseconds. std::chrono is used here, but any
@@ -17,17 +19,22 @@ mcap::Timestamp now() {
 }
 
 void fdSetInternal(google::protobuf::FileDescriptorSet& fd_set,
+                   std::unordered_set<std::string> files,
                    const google::protobuf::FileDescriptor* fd) {
   for (int i = 0; i < fd->dependency_count(); ++i) {
-    fdSetInternal(fd_set, fd->dependency(i));
+    const auto* dep = fd->dependency(i);
+    auto [_, inserted] = files.insert(dep->name());
+    if (!inserted) continue;
+    fdSetInternal(fd_set, files, fd->dependency(i));
   }
   fd->CopyTo(fd_set.add_file());
 }
 
 std::string fdSet(const google::protobuf::Descriptor* d) {
   std::string res;
+  std::unordered_set<std::string> files;
   google::protobuf::FileDescriptorSet fd_set;
-  fdSetInternal(fd_set, d->file());
+  fdSetInternal(fd_set, files, d->file());
   return fd_set.SerializeAsString();
 }
 
@@ -38,31 +45,62 @@ int main() {
 
   // Initialize an MCAP writer with the "ros1" profile and write the file header
   mcap::McapWriter writer;
-  writer.open(out, mcap::McapWriterOptions("protobuf"));
+  mcap::McapWriterOptions opts("protobuf");
+  opts.compressionLevel = mcap::CompressionLevel::Fast;
+  writer.open(out, opts);
 
-  // Register a Schema
-  mcap::Schema stdMsgsString("ros.std_msgs.String", "proto",
-                             fdSet(ros::std_msgs::String::descriptor()));
-  writer.addSchema(stdMsgsString);
+  {
+    // Register a Schema
+    mcap::Schema stdMsgsString("ros.std_msgs.String", "proto",
+                               fdSet(ros::std_msgs::String::descriptor()));
+    writer.addSchema(stdMsgsString);
 
-  // Register a Channel
-  mcap::Channel chatterPublisher("/chatter", "protobuf", stdMsgsString.id);
-  writer.addChannel(chatterPublisher);
+    // Register a Channel
+    mcap::Channel chatterPublisher("/chatter", "protobuf", stdMsgsString.id);
+    writer.addChannel(chatterPublisher);
 
-  ros::std_msgs::String s;
-  s.set_data("foo");
+    ros::std_msgs::String s;
+    s.set_data("foo");
 
-  std::string payload = s.SerializeAsString();
+    std::string payload = s.SerializeAsString();
 
-  // Write our message
-  mcap::Message msg;
-  msg.channelId = chatterPublisher.id;
-  msg.sequence = 1;               // Optional
-  msg.logTime = now();            // Required nanosecond timestamp
-  msg.publishTime = msg.logTime;  // Set to logTime if not available
-  msg.data = reinterpret_cast<std::byte*>(payload.data());
-  msg.dataSize = payload.size();
-  writer.write(msg);
+    // Write our message
+    mcap::Message msg;
+    msg.channelId = chatterPublisher.id;
+    msg.sequence = 1;               // Optional
+    msg.logTime = now();            // Required nanosecond timestamp
+    msg.publishTime = msg.logTime;  // Set to logTime if not available
+    msg.data = reinterpret_cast<std::byte*>(payload.data());
+    msg.dataSize = payload.size();
+    writer.write(msg);
+  }
+
+  {
+    // Register a Schema
+    mcap::Schema schema("ros.geometry_msgs.PointStamped", "proto",
+                        fdSet(ros::geometry_msgs::PointStamped::descriptor()));
+    writer.addSchema(schema);
+
+    // Register a Channel
+    mcap::Channel chatterPublisher("/point", "protobuf", schema.id);
+    writer.addChannel(chatterPublisher);
+
+    ros::geometry_msgs::PointStamped p;
+    p.mutable_header()->set_frame_id("/map");
+    p.mutable_point()->set_x(1.0);
+
+    std::string payload = p.SerializeAsString();
+
+    // Write our message
+    mcap::Message msg;
+    msg.channelId = chatterPublisher.id;
+    msg.sequence = 1;               // Optional
+    msg.logTime = now();            // Required nanosecond timestamp
+    msg.publishTime = msg.logTime;  // Set to logTime if not available
+    msg.data = reinterpret_cast<std::byte*>(payload.data());
+    msg.dataSize = payload.size();
+    writer.write(msg);
+  }
 
   // Finish writing the file
   writer.close();
